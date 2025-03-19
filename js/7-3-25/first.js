19864dea8e9a0a920086ca3ad808814a89fb9c21
abhishekparvam <abhishekganesh.parvam@gmail.com>
2025-03-19 19:53:34 +0530

    selecting elements & text content

---
+++ js/7-3-25/first.js
@@ -0,0 +1,21 @@
+//console.log("Hello");
+//console.log(document.getRootNode());
+const document_root = document.getRootNode();
+console.log(document_root.childNodes);
+const html_tag = document_root.childNodes[1];
+console.log(html_tag);
+console.log(html_tag.childNodes);
+const head_tag = html_tag.childNodes[0];
+console.log(head_tag.childNodes);
+console.log(head_tag.children);
+const title_tag = head_tag.children[0];
+console.log(title_tag.innerText);
+console.log(title_tag.textContent);
+console.log(title_tag.innerHTML);
+console.log(title_tag.parentNode);
+console.log(title_tag.nextSibling);
+console.log(title_tag.nextElementSibling);
+
+//const body_tag = html_tag.childNodes[2];
+const body_tag = document.body;
+console.log(body_tag);
