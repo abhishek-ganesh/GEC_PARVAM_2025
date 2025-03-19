5e5ae9cb548be40005c59fb0e837dce79f2fa95b
abhishekparvam <abhishekganesh.parvam@gmail.com>
2025-03-19 19:50:32 +0530

    django CRUD

---
+++ django/gec_crud/std_crd/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from . import models
+
+# Register your models here.
+
+
+@admin.register(models.Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "email", "password"]
